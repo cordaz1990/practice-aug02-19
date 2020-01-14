93cb55a183ff75b7fe925cce9fa2400f62e48ba6
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 22:05:11 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,19 @@
-print('hello')
-print('world')
-print('hello')
+def add_all(t):
+    total = 0
+    for x in t:
+      total += x
+    return total
+  
+ def capitalize_all(t):
+     res = []
+     for s in t:
+         res.apped(s.capitalize())
+     return res
+  
+  
+  def only_upper(t):
+      res = []
+      for s in t:
+        if s.isupper():
+          res.append(s)
+      return res
